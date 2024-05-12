397c40f1394ab3164fbf2a0e3d074062f6dde6e9
slmthang <163381046+slmthang@users.noreply.github.com>
2024-05-12 04:43:15 -0400

    frontend

---
+++ frontend/src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+import "./css/default.css";
+import "./scss/styles.scss";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
